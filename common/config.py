5d8b49dc0bc3765c841b9b8a18d3e22f81f24aaa
Unknown <pat.hearps@gmail.com>
2022-10-28 21:01:56 +1100

    CHORE: delete stuff related to old API

--- common/config.py
+++ common/config.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from datetime import datetime
 import os
 
 DBPASS = os.getenv("POSTGRES_PASSWORD", default="")
@@ -11,16 +10,3 @@ DBHOST = os.getenv("DB_HOST", default="localhost")
 DBPORT = os.getenv("DB_PORT", default="5432")
 
 DB_URL = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}:{DBPORT}/{DBNAME}"
-
-HTTPS = "https://"
-HOST_EPIC = "epic.gsfc.nasa.gov"
-API = "api"
-ARCHIVE = "archive"
-NATURAL = "natural"
-ENHANCED = "enhanced"
-PNG = "png"
-JPG = "jpg"
-BASE_ADDR = f"{HTTPS}{HOST_EPIC}"
-
-INIT_DATE = datetime(2021, 1, 1)
-
