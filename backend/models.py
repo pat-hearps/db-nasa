aeab89ad64095dbe698339b4f01ba41c89e60054
Unknown <pat.hearps@gmail.com>
2022-10-28 22:31:13 +1100

    FEAT: map out basic raw tables for articles

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,28 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, Float, String, BigInteger, Integer
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
 
 
-class EpicMeta(Base):
-    __tablename__ = "epic_metadata"
+class ArticleMeta(Base):
+    """Contains identifying information about an article"""
+    __tablename__ = "meta"
     id = Column(Integer, primary_key=True)
-    identifier = Column(BigInteger)
-    taken_datetime = Column(DateTime)
-    version = Column(String(length=6))
-    image_name = Column(String(length=30))
-    centroid_lat = Column(Float)
-    centroid_long = Column(Float)
+    section = Column(String, length=80)
+    title = Column(String)
+    identifier = Column(String)
+    published_datetime = Column(DateTime)
+
+    articles = relationship("Article")
+
+class Article(Base):
+    """Contains actual content of articles,
+    trail: trailing text
+    body: main article content"""
+    __tablename__ = "articles"
+    id = Column(Integer, primary_key=True)
+    meta_id = Column(ForeignKey("meta.id"))
+    trail = Column(String)
+    body = Column(String)
